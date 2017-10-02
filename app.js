9e8bb8bcc4930f1264f45fa3ad92af7102520eac
Kayin McLeod <kayin.angel@gmail.com>
2017-10-02 15:47:24 -0400

    Psh

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const express      = require('express'),
 
 const app = express();
 
-let env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
